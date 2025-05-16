58468212b2b6365d81bfb967e1f2302d9e120cd3
felipesantos2 <santospinheiro627@gmail.com>
2025-05-16 20:44:54 -0300

    add app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
-// selecionar texo em inputs 
+// selecionar texo em inputs
 // converter entre caixa alta e baixa
-// validar se o tipo do input está errado
+// validar se o tipo do input está errado e adicionae warnings
+// gerar dados fake?
